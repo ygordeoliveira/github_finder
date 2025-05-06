70681b9bb38dc0717cd41326550bd9c03e918a5f
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2025-05-06 19:51:41 -0300

    File of project

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Pages
+import Home from './routes/Home.tsx';
+import Repos from './routes/Repos.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+              path: "/repos/:username",
+              element: <Repos />,
+            },
+        ]
+    },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
